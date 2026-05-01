0ad2d74250d1205f399bd65299818d39a2cdd495
karl5252 <karl.michalik@gmail.com>
2026-05-01 17:33:37 +0200

    feat: update player implementation with sprite and physics integration

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,6 +1,6 @@
 import Phaser from "phaser";
-import { logger } from "../utils/Logger.js";
-import { Player } from "../Player.js";
+import {logger} from "../utils/Logger.js";
+import {Player} from "../Player.js";
 import Bear from "../Bear.js";
 
 export class GameScene extends Phaser.Scene {
@@ -12,12 +12,8 @@ export class GameScene extends Phaser.Scene {
     const { width, height } = this.scale;
 
     this.player = new Player(this, width / 2, height / 2);
-    // Ensure player starts within bounds
-    this.player.x = Phaser.Math.Clamp(this.player.x, 0, this.scale.width);
-   this.player.y = Phaser.Math.Clamp(this.player.y, 0, this.scale.height);
-    // after sprite replace with rectangle, we need to set the physics body manually
-    //this.player = this.scene.physics.add.image(width / 2, height / 2, 'car');
-    this.bear = new Bear(
+
+      this.bear = new Bear(
       this,
       Phaser.Math.Between(0, this.scale.width),
       Phaser.Math.Between(0, this.scale.height),
